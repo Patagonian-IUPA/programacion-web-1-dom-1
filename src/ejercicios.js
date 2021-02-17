5db203ced890fb06179fd6a945b88b0fcdcd6d1e
Lucero Amaolo <michilnx@gmail.com>
2021-02-16 21:43:43 -0300

    Ejercicios resueltos

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,1 +1,21 @@
 // @TODO Completar...
+
+const contenedor = document.getElementById("contenedor");
+
+const lista = document.createDocumentFragment();
+
+const unorderedList = document.createElement("ul");
+
+lista.appendChild(unorderedList);
+
+const template = document.getElementById("template").content;
+
+for (let i = 1; i <= 50; i++) {
+  const listItem = template.getElementById("list-item");
+  listItem.innerText = i;
+
+  const nodo = template.cloneNode(true);
+  unorderedList.appendChild(nodo);
+}
+
+contenedor.appendChild(lista);
