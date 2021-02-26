5095eb4ee58b65b11ddeade55b1a2b30a1c5d05f
Juan Pablo Toribio <juanpablotoribio@gmail.com>
2021-02-26 00:12:07 -0300

    Ejercicios

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,1 +1,17 @@
 // @TODO Completar...
+
+const contenedor = document.getElementById("contenedor");
+
+const lista = document.createDocumentFragment();
+
+const ul = document.createElement("ul");
+
+lista.appendChild(ul);
+
+for (let i = 0; i < 50; i++) {
+    const li = document.createElement("li");
+    li.textContent = i;
+    ul.appendChild(li);
+  }
+
+contenedor.appendChild(lista);
