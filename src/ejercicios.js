e42ccc421b5d5d49c0c4961e01fefdb4f90ef284
Aldo <aldocape@gmail.com>
2021-03-03 16:58:23 -0300

    Cambio el nombre de la variable 'elem' a 'itemList' y saco el método cloneNode que estaba afuera del for

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -7,27 +7,24 @@ const contenedor = document.getElementById('contenedor');
 const lista = document.createDocumentFragment();
 
 // Creo el ul dinámicamente y lo almaceno en el DocumentFragment
-const itemList = document.createElement('ul');
-itemList.setAttribute('id', 'itemList');
-
-lista.appendChild(itemList);
+const ul = document.createElement('ul');
+lista.appendChild(ul);
 
 // Selecciono el template (agregado a mano en el html), y lo asigno a la variable 'tt'
 const tt = document.getElementById('mitemplate');
 
-// Busco el elemento 'li' dentro del template y guardo su referencia en la variable 'elem'
-const elem = tt.content.querySelector('li');
-
-// Declaro la variable con let y no const, porque adentro del for le reasigno valores 
-let nodo = elem.cloneNode(true);
+// Busco el elemento 'li' dentro del template y guardo su referencia en la variable 'itemList'
+const itemList = tt.content.querySelector('li');
 
 for (let i = 1; i <= 50; i++) {
-    nodo.textContent = `Elemento nº: ${i}`;
-    // Inserto el 'li' que guardé en la variable 'elem', en la lista 'ul' del template
-    itemList.insertBefore(nodo, itemList.firstElement);
-
     // Utilizo el método cloneNode para clonar el elemento que obtengo del template y poder usarlo
-    nodo = elem.cloneNode(true);
+    const nodo = itemList.cloneNode(true);
+
+    // Agrego texto que identifique el número de línea que se agrega
+    nodo.textContent = `Elemento nº: ${i}`;
+    
+    // Inserto el 'li' que guardé en la variable 'elem', en la lista 'ul' del template
+    ul.insertBefore(nodo, ul.firstElement);
 }
 
 // Finalmente, agrego el DocumentFragment modificado dentro del div contenedor, dinámicamente
