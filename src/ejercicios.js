862ad0309653a3497af600fea0f553bd450c5962
Juan Pablo Toribio <juanpablotoribio@gmail.com>
2021-03-04 10:18:04 -0300

    Correcciones 2.0

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,17 +1,25 @@
 // @TODO Completar...
 
+//1
 const contenedor = document.getElementById("contenedor");
 
+//2
 const lista = document.createDocumentFragment();
 
+//3
 const ul = document.createElement("ul");
 
 lista.appendChild(ul);
 
+//5
+const template = document.getElementById("mitemplate").content;
+
 for (i = 0; i < 50; i++) {
-    const li = `<li class="list-group">${i}</li>`;
-    ul.innerHTML += li;
+    const li = template.getElementById("list");
+    li.textContent = i;
+    const nodo = template.cloneNode(true);
+    lista.appendChild(nodo);
     }
 
-
-contenedor.appendChild(lista);
+//6
+    contenedor.appendChild(lista);
