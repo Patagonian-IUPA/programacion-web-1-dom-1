52b648a20e58c3181e7b33ca10e8f8b0fe6c91e7
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2021-03-08 00:50:15 -0300

    Agregando Fragment y Template

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,1 +1,19 @@
-// @TODO Completar...
+'use strict'
+
+const contenedor = document.querySelector('#contenedor');
+
+const lista = document.createDocumentFragment();
+
+const ul = document.createElement('ul');
+
+lista.appendChild(ul);
+
+const template = document.querySelector('#template').content;
+
+for (let i = 1; i <= 50; i++) {
+    const li = template.querySelector('li');
+    li.textContent = `Elemento: ${i}`;
+    const item = template.cloneNode(true);
+    ul.appendChild(item);
+}
+contenedor.appendChild(lista);
