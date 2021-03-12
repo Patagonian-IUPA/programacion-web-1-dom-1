3ac1ca45e01db73e3edd66d7fd2c209c99b7f693
Lausi Juan Pablo <juanpablo_lausi@hotmail.com.ar>
2021-03-12 15:08:17 -0300

    codigo para correccion

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,1 +1,19 @@
 // @TODO Completar...
+const contenedor = document.getElementById("contenedor");
+
+const lista = document.createDocumentFragment();
+
+const ul = document.createElement("ul");
+lista.appendChild(ul);
+
+const template = document.getElementById("idtemplate");
+
+const item = template.content.querySelector('li');
+
+for (let index = 1; index <= 50; index++) {
+    const itemNuevo = item.cloneNode(true);
+    itemNuevo.textContent = index;
+    ul.appendChild(itemNuevo);
+}
+
+contenedor.appendChild(lista);
