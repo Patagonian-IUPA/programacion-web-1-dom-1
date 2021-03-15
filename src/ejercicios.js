18395855fd657b2ad2485a7f34539aee2afa2242
gdsasso <71105600+gdsasso@users.noreply.github.com>
2021-03-14 23:57:52 -0300

    Codigo a Corregir

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -1,1 +1,21 @@
 // @TODO Completar...
+const contenedor = document.getElementById("contenedor");
+console.log (contenedor);
+
+const lista = document.createDocumentFragment();
+console.log (lista);
+
+const ul = document.createElement("ul");
+lista.appendChild(ul);
+
+const template = document.getElementById("idtemplate");
+
+const item = template.content.querySelector("li");
+
+for (let index = 1; index <= 50; index++) {
+    const itemNew = item.cloneNode(true);
+    itemNew.textContent = index;
+    ul.appendChild(itemNew);
+}
+
+contenedor.appendChild(lista);
